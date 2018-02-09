f3eda3325911f9aebde90c30a7bf5af6c6214d21
markwilkinson <markw@illuminae.com>
2018-02-09 11:24:32 +0100

    misc

--- MetricsEvaluatorCode/Ruby/fairmetrics/config/routes.rb
+++ MetricsEvaluatorCode/Ruby/fairmetrics/config/routes.rb
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
    
-  root 'static_pages#about'
-  get '/about', to: 'static_pages#about'
+  root 'static_pages#show'
+  get '/about', to: 'static_pages#show'
   
     
   get 'metric', to: 'metrics#index'
