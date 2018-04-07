25d134c30862e27788a4a7d48abd46799123d2d1
Carlos <c.martinez@esciencecenter.nl>
2018-04-07 12:20:44 +0200

    Generate pdf via jinja template

--- script.py
+++ script.py
@@ -1,28 +1,32 @@
+import os
+import sys
+import jinja2
+
 from rdflib import ConjunctiveGraph, URIRef
 from rdflib.namespace import DCTERMS, RDFS, FOAF
 from rdflib.namespace import Namespace
 
-fairGraph = ConjunctiveGraph()
-# fairGraph.parse('http://purl.org/fair-ontology#', format='trig')
-
-g = ConjunctiveGraph()
-g.parse('FM_A1.1', format='trig')
-#for ctx in g.contexts():
-#    print ctx.identifier, '----------'
-#    for s,p,o in ctx:
-#        print s,p,o
-#    print '=================================='
+args = sys.argv
+if len(args)!=2:
+    raise Exception('Expected metric IRI as input')
 
 FM = Namespace('https://purl.org/fair-metrics/terms/')
 
+fairGraph = ConjunctiveGraph()
+fairGraph.parse('http://purl.org/fair-ontology#', format='trig')
+
+fairTermGraph = ConjunctiveGraph()
+fairTermGraph.parse('terms', format='n3')
 
 class FairMetricData():
     def __init__(self, id):
         self.base = 'https://purl.org/fair-metrics/'
         self.id = URIRef(id)
-        print "TODO: really should parse g from ID"
-        self.g = g
         self.assertion = URIRef(id+'#assertion')
+
+        id = id.replace(self.base, '')  # HACK -- remove this line before merging commit
+        self.g = ConjunctiveGraph()
+        self.g.parse(id, format='trig')
 
     def getID(self):
         return self.id
@@ -31,86 +35,136 @@ class FairMetricData():
         return self.id.replace(self.base, '')
 
     def getAuthors(self):
-        return [o for o in self.g.objects(subject=self.assertion, predicate=DCTERMS.author)]
+        authors = [o.toPython() for o in self.g.objects(subject=self.assertion, predicate=DCTERMS.author)]
+        authors.sort()
+        return ' \\\\ '.join(authors)
 
     def getTitle(self):
-        return [o for o in self.g.objects(subject=self.assertion, predicate=RDFS.comment)]
+        return ', '.join([o.toPython() for o in self.g.objects(subject=self.assertion, predicate=RDFS.comment)])
 
     def getShortTitle(self):
-        return [o for o in self.g.objects(subject=self.assertion, predicate=DCTERMS.title)]
+        return ', '.join([o.toPython() for o in self.g.objects(subject=self.assertion, predicate=DCTERMS.title)])
 
     def getTopicDescription(self):
         descs = []
         for o in self.g.objects(subject=self.id, predicate=FOAF.primaryTopic):
             # o should be fair:A1.1
             for o2 in fairGraph.objects(subject=o, predicate=DCTERMS.description):
-                descs.append(o2)
-        return descs
+                descs.append(o2.toPython())
+        return ' '.join(descs)
 
     def getTopicTitle(self):
         descs = []
         for o in self.g.objects(subject=self.id, predicate=FOAF.primaryTopic):
             # o should be fair:A1.1
             for o2 in fairGraph.objects(subject=o, predicate=DCTERMS.title):
-                descs.append(o2)
-        return descs
+                descs.append(o2.toPython())
+        return ' '.join(descs)
 
     def getMeasuring(self):
         # return fm:measuring
-        pred = FM.measuring
-        return [o for o in self.g.objects(subject=self.assertion, predicate=pred)]
+        return self.getFMPropertyValue(FM.measuring)
 
     def getReason(self):
         # return fm:reason
-        pred = FM.reason
-        return [o for o in self.g.objects(subject=self.assertion, predicate=pred)]
+        return self.getFMPropertyValue(FM.reason)
 
     def getRequirements(self):
         # return fm:requirements
-        pred = FM.requirements
-        return [o for o in self.g.objects(subject=self.assertion, predicate=pred)]
+        return self.getFMPropertyValue(FM.requirements)
 
     def getProcedure(self):
         # return fm:procedure
-        pred = FM.procedure
-        return [o for o in self.g.objects(subject=self.assertion, predicate=pred)]
+        return self.getFMPropertyValue(FM.procedure)
 
     def getValidation(self):
         # return fm:validation
-        pred = FM.validation
-        return [o for o in self.g.objects(subject=self.assertion, predicate=pred)]
+        return self.getFMPropertyValue(FM.validation)
 
     def getRelevance(self):
         # return fm:relevance
-        pred = FM.relevance
-        return [o for o in self.g.objects(subject=self.assertion, predicate=pred)]
+        return self.getFMPropertyValue(FM.relevance)
 
     def getExamples(self):
         # return fm:examples
-        pred = FM.examples
-        return [o for o in self.g.objects(subject=self.assertion, predicate=pred)]
+        return self.getFMPropertyValue(FM.examples)
 
     def getComments(self):
         # return fm:comments
-        pred = FM.comments
-        return [o for o in self.g.objects(subject=self.assertion, predicate=pred)]
+        return self.getFMPropertyValue(FM.comments)
+
+    def getFMPropertyLabel(self, property):
+        return ', '.join([ o.toPython() for o in fairTermGraph.objects(subject=FM[property], predicate=RDFS['label'])])
+
+    def getFMPropertyValue(self, property):
+        return ', '.join([o.toPython() for o in self.g.objects(subject=self.assertion, predicate=property)])
 
 
 # The idea is that we could fill the table http://fairmetrics.org/fairmetricform.html
 # from a given metric IRI
-id = 'https://purl.org/fair-metrics/FM_A1.1'
+# id = 'https://purl.org/fair-metrics/FM_A1.1'
+metricFile = args[1]
+id = 'https://purl.org/fair-metrics/' + metricFile
+
 fm = FairMetricData(id)
-print fm.getShortID() + " - " + fm.getID()
-print fm.getAuthors()
-print fm.getTitle()
-print fm.getShortTitle()
-print fm.getTopicTitle()
-print fm.getTopicDescription()
-print fm.getMeasuring()
-print fm.getReason()
-print fm.getRequirements()
-print fm.getProcedure()
-print fm.getValidation()
-print fm.getRelevance()
-print fm.getExamples()
-print fm.getComments()
+
+latex_jinja_env = jinja2.Environment(
+	variable_start_string = '\VAR{',
+	variable_end_string = '}',
+	trim_blocks = True,
+	autoescape = False,
+	loader = jinja2.FileSystemLoader(os.path.abspath('.'))
+)
+template = latex_jinja_env.get_template('template.tex')
+
+
+title=fm.getTitle()
+authors=fm.getAuthors()
+metricId=fm.getShortID().replace('_','-')   # Avoid _ in latex template
+metricIdVerb=fm.getID()
+shortTitle=fm.getShortTitle()
+topicTitle=fm.getTopicTitle()
+topicDesription=fm.getTopicDescription()
+measuring=fm.getMeasuring()
+reason=fm.getReason()
+requirements=fm.getRequirements().replace('\n','\\newline\n')
+procedure=fm.getProcedure()
+validation=fm.getValidation()
+relevance=fm.getRelevance()
+examples=fm.getExamples()
+comments=fm.getComments()
+measuringLabel=fm.getFMPropertyLabel('measuring')
+reasonLabel=fm.getFMPropertyLabel('reason')
+requirementsLabel=fm.getFMPropertyLabel('requirements')
+procedureLabel=fm.getFMPropertyLabel('procedure')
+validationLabel=fm.getFMPropertyLabel('validation')
+relevanceLabel=fm.getFMPropertyLabel('relevance')
+examplesLabel=fm.getFMPropertyLabel('examples')
+commentsLabel=fm.getFMPropertyLabel('comments')
+
+
+print(template.render(
+    title=title,
+    authors=authors,
+    metricId=metricId,
+    metricIdVerb=metricIdVerb,
+    shortTitle=shortTitle,
+    topicTitle=topicTitle,
+    topicDesription=topicDesription,
+    measuring=measuring,
+    reason=reason,
+    requirements=requirements,
+    procedure=procedure,
+    validation=validation,
+    relevance=relevance,
+    examples=examples,
+    comments=comments,
+    measuringLabel=measuringLabel,
+    reasonLabel=reasonLabel,
+    requirementsLabel=requirementsLabel,
+    procedureLabel=procedureLabel,
+    validationLabel=validationLabel,
+    relevanceLabel=relevanceLabel,
+    examplesLabel=examplesLabel,
+    commentsLabel=commentsLabel,
+))
