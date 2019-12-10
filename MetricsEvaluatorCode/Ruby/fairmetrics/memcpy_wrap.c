b83ddaf97350a49059b3718874f42ad56920174f
Mark Wilkinson <markw@illuminae.com>
2019-12-10 11:14:09 +0000

    oops, added cruft to the folder.  removed now

--- MetricsEvaluatorCode/Ruby/fairmetrics/memcpy_wrap.c
+++
@@ -1,7 +0,0 @@
-#include <stddef.h>
-#include <string.h>
-
-asm (".symver wrap_memcpy, memcpy@GLIBC_2.2.5");
-void *wrap_memcpy(void *dest, const void *src, size_t n) {
-  return memcpy(dest, src, n);
-}
